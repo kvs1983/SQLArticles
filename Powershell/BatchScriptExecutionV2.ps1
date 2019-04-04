ca5fb09cafb93839f2b75f475ae1aff17057be86
Vidhya Sagar <kvs1983@gmail.com>
2019-04-04 22:57:23 +0200

    Updated script as a function with synopsis

--- Powershell/BatchScriptExecutionV2.ps1
+++ Powershell/BatchScriptExecutionV2.ps1
@@ -1,60 +1,88 @@
-$error.clear()
-$Server = Read-Host -Prompt 'Enter Server Name'
-$DatabaseName = Read-Host -Prompt 'Enter Database Name'
-$UserName = Read-Host -Prompt 'Enter Windows or SQL User Name'
-$Password = Read-Host -AsSecureString -Prompt 'Enter Password'
+<#
+ .SYNOPSIS Execute SQL Script in order
+ .Parameter Server Provide SQL Server Name
+ .Parameter DatabaseName Provide database name
+ .Parameter UserName Provide a windows user name or SQL Server user name
+ .Parameter Password Provide password as a secure string
+ .Parameter ScriptPath Provide the path where all the SQL scripts are located
+ .Parameter ContineOnException Enable this switch to continue even if one of the script failed to execute
+ .EXAMPLE 
+ $SecurePass = ConvertTo-SecureString "MyPassword"
+ Invoke-BatchSqlExecution -Server "MyServer" -DatabaseName "MyDatabasae" -UserName "MyUserId" -Password $SecurePass -ScriptPath "C:\Temp\SQL" -ContinueOnException
+#>
+function Invoke-BatchSqlExecution { 
+    param(
+        [parameter(Mandatory = $True, Position = 0)]
+        [string]$Server,
+        [parameter(Mandatory = $True, Position = 1)]
+        [string]$DatabaseName,
+        [parameter(Mandatory = $True, Position = 2)]
+        [string]$UserName,
+        [parameter(Mandatory = $True, Position = 3)]
+        [SecureString]$Password,
+        [parameter(Mandatory = $True, Position = 3)]
+        [string]$ScriptPath,
+        [parameter(Mandatory = $False, Position = 3)]
+        [switch]$ContinueOnException
+    )
 
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, $Password 
+    $error.clear()
 
-$ScriptPath = Read-Host -Prompt 'Enter relative path'
-$windowsusername = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.ToString()
-$Datetime = Get-Date
-$processstarttime = $Datetime.ToString("yyyyMMddHHmmssfff")
-$logfile = "$ScriptPath\executionlog_$processstarttime.log"
+    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $Username, $Password 
 
-Start-Transcript -Path $logfile
-Write-Host  "Batch Execution Date   : $Datetime"
-Write-Host  "Server Name            : $server" 
-Write-Host  "Database Name          : $DatabaseName"
-Write-Host  "User Name              : $UserName"
-Write-Host  "Executed By            : $windowsusername" 
-Write-Host  "Script Path            : $ScriptPath"
-Write-Host  "Log File               : $logfile"
-Write-Host  $("*" * 100) -ForegroundColor Cyan
+    $windowsusername = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.ToString()
+    $Datetime = Get-Date
+    $processstarttime = $Datetime.ToString("yyyyMMddHHmmssfff")
+    $logfile = "$ScriptPath\executionlog_$processstarttime.log"
 
-$checkmodule = Get-Module -ListAvailable | Where-Object { $_.Name  -eq "dbatools"}
-if (!$checkmodule) {    
-    Write-Host "Installing dbatools module" -ForegroundColor Cyan
-    Start-Process powershell.exe -ArgumentList "-Command Install-Module dbatools" -Verb RunAs -Wait
-} elseif ($checkmodule) {
-    Import-Module dbatools -Force
-    Write-Host "dbatools module already exists" -ForegroundColor Green
-}
+    Start-Transcript -Path $logfile
+    Write-Host  "Batch Execution Date   : $Datetime"
+    Write-Host  "Server Name            : $server" 
+    Write-Host  "Database Name          : $DatabaseName"
+    Write-Host  "User Name              : $UserName"
+    Write-Host  "Executed By            : $windowsusername" 
+    Write-Host  "Script Path            : $ScriptPath"
+    Write-Host  "Log File               : $logfile"
+    Write-Host  $("*" * 100) -ForegroundColor Cyan
 
-if (!([System.IO.File]::Exists($ScriptPath))){
-    Write-Host "$ScriptPath does not exists or the user does not have access" -ForegroundColor Red
-    Break
-}
+    $checkmodule = Get-Module -ListAvailable | Where-Object { $_.Name -eq "dbatools" }
+    if (!$checkmodule) {    
+        Write-Host "Installing dbatools module" -ForegroundColor Cyan
+        Start-Process powershell.exe -ArgumentList "-Command Install-Module dbatools" -Verb RunAs -Wait
+    }
+    elseif ($checkmodule) {
+        Import-Module dbatools -Force
+        Write-Host "dbatools module already exists" -ForegroundColor Green
+    }
+
+    if (!(Test-Path $ScriptPath)) {
+        Write-Host "$ScriptPath does not exists or the user does not have access" -ForegroundColor Red
+        Break
+    }
                
-foreach ($file in Get-ChildItem $ScriptPath -Filter "*.sql" -Recurse | Sort-Object -Property FullName) {
-    try {
-        Write-Host "****** PROCESSING $file FILE ******" -ForegroundColor Yellow
-        Invoke-DbaQuery -SqlInstance $Server `
-            -SqlCredential $Credential `
-            -Database $DatabaseName `
-            -File $file.FullName `
-            -EnableException `
-            -MessagesToOutput
-        Write-Host "******SUCCESSFULLY PROCESSED $file FILE ******" -ForegroundColor Green  
-        Write-Host $("*" * 100) 
+    foreach ($file in Get-ChildItem $ScriptPath -Filter "*.sql" -Recurse | Sort-Object -Property FullName) {
+        try {
+            Write-Host "****** PROCESSING $file FILE ******" -ForegroundColor Yellow
+            Invoke-DbaQuery -SqlInstance $Server `
+                -SqlCredential $Credential `
+                -Database $DatabaseName `
+                -File $file.FullName `
+                -EnableException `
+                -MessagesToOutput
+            Write-Host "******SUCCESSFULLY PROCESSED $file FILE ******" -ForegroundColor Green  
+            Write-Host $("*" * 100) 
                 
+        }
+        catch {
+            Write-Host "******FAILED PROCESSING $file FILE ******" -ForegroundColor Red
+            Write-Host $($Error[0].Exception.Message) -ForegroundColor Red
+            Write-Host $("*" * 100)
+            IF ($ContinueonException.IsPresent) {
+                continue
+            }
+            else { throw }
+        }
     }
-    catch {
-        Write-Host "******FAILED PROCESSING $file FILE ******" -ForegroundColor Red
-        Write-Host "$error[0].Exception.Message" -ForegroundColor Red
-        Write-Host $("*" * 100)
-
-    }
+    Write-Host "****** All Files Processed Successfully ******" -ForegroundColor Green
+    Stop-Transcript
 }
-Write-Host "****** All Files Processed Successfully ******" -ForegroundColor Green
-Stop-Transcript
